2d3743004968b91931100dee27a3df58da76bd97
jeromepacman <jeromepacman@gmail.com>
2021-06-11 22:20:29 +0200

    home page

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
-import os,sys
+import os, sys
 
 virtenv = os.path.expanduser('~') + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 
 try:
-  if sys.version.split(' ')[0].split('.')[0] == '3':
-    exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
-  else:
-    execfile(virtualenv, dict(__file__=virtualenv))
+    if sys.version.split(' ')[0].split('.')[0] == '3':
+        exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+    else:
+        execfile(virtualenv, dict(__file__=virtualenv))
 except IOError:
-  pass
+    pass
 
 sys.path.append(os.path.expanduser('~'))
 sys.path.append(os.path.expanduser('~') + '/ROOT/')
@@ -17,4 +17,5 @@ sys.path.append(os.path.expanduser('~') + '/ROOT/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ROOT.tview.settings'
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()

24c6ed60487a0affe7f0aa2fe6731c6dcba342bf
jeromepacman <jeromepacman@gmail.com>
2022-06-09 13:36:38 +0200

    integration JS to Django suite

--- pages/urls.py
+++ pages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
-    path('you/', views.PersoView.as_view(), name="perso"),
-    path('shop/', views.ShopView.as_view(), name="boutique"),
-    path('applications/', views.ProView.as_view(), name="pro"),
+    path('', views.HomeView.as_view(), name="home"),
+    path('perso/', views.PersoView.as_view(), name="perso"),
+    path('boutique/', views.ShopView.as_view(), name="boutique"),
+    path('pro/', views.ProView.as_view(), name="pro"),
     path('infos/', views.InfosView.as_view(), name="infos"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import TemplateView
 
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class HomeView(TemplateView):
+    template_name = "home.html"
 
 
 class PersoView(TemplateView):
